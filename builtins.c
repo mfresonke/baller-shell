ba10350632fe06e1e40a31f65d1661582a9b961b
Max Fresonke <max@fresonke.com>
2015-03-12 15:49:41 -0400

    Fixed default CD behaviour, and fixed bug with escaped lines within quotes.

--- builtins.c
+++ builtins.c
@@ -2,6 +2,11 @@
 
 void cd( char *dir )
 {
+	//if dir is NULL (empty) then by default we go back to home.
+	if( !dir )
+	{
+		dir = getenv( "HOME" );
+	}
 	//change enviornment variables
 	setenv( "OLDPWD", getenv("PWD"), 1 );
 	setenv( "PWD", dir, 1 );
